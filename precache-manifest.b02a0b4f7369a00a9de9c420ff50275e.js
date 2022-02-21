9577023849648bbb64661abcca5e0b665e7ab201
srgbar <srg.bar@yandex.ru>
2022-02-22 02:44:53 +0300

    Updates

---
+++ precache-manifest.b02a0b4f7369a00a9de9c420ff50275e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2565f1bb510642e3881b508fb82fed78",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "d1bcda3a53b84760608d",
+    "url": "/counter/static/css/main.a8c61c41.chunk.css"
+  },
+  {
+    "revision": "21015a18efd6e31b51c0",
+    "url": "/counter/static/js/2.f82110f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/counter/static/js/2.f82110f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bcda3a53b84760608d",
+    "url": "/counter/static/js/main.18a44511.chunk.js"
+  },
+  {
+    "revision": "41ab835c22a2ec780517",
+    "url": "/counter/static/js/runtime-main.c9bcdafb.js"
+  }
+]);
