ccc19350f9d9231f27ba9db988492d01239bf41f
srgbar <srg.bar@yandex.ru>
2022-02-23 01:54:39 +0300

    added the functionality of saving the starting and maximum counter values in Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './App.module.css';
 import RootComponent from "./components/RootComponent";
 
 const App = () => {
 
     const [countValue, setCountValue] = useState<number>(0) // текущее значение счетчика
-    const [startInputValue, setStartInputValue] = useState<number>(0) // стартовое значение счетчика
+    const [startInputValue, setStartInputValue] = useState(0) // стартовое значение счетчика
     const [maxInputValue, setMaxInputValue] = useState<number>(5) // макс значение счетчика
     const [isMessage, setIsMessage] = useState<boolean>(true) // показывать или нет сообщения вместо value
-
     const condition = startInputValue < 0 || startInputValue > maxInputValue || startInputValue === maxInputValue // проверка на условия ввода
 
-    const incData = () => {  // увеличение счетчика на единицу
-        if (startInputValue < maxInputValue) {
-            setCountValue(countValue + 1);
+    // сохраняем стартовое и максимальное значения счетчика в Local Storage
+    useEffect(() => {
+        const localStorageStartValueStr = localStorage.getItem("startInputValue")
+        const localStorageMaxValueStr = localStorage.getItem("maxInputValue")
+
+        if (localStorageStartValueStr) {
+            setStartInputValue(JSON.parse(localStorageStartValueStr))
         }
+        if (localStorageMaxValueStr) {
+            setMaxInputValue(JSON.parse(localStorageMaxValueStr))
+        }
+    }, [])
+
+    const incData = () => {  // увеличение счетчика на единицу
+        setCountValue(countValue + 1)
     }
 
     const resData = () => { // сброс значения счетчика до стартового
-        setCountValue(startInputValue);
+        setCountValue(startInputValue)
     }
 
     const onClickSettings = () => { // передача настроек счетчика
         setCountValue(startInputValue)
         setMaxInputValue(maxInputValue)
         setIsMessage(true)
+        localStorage.setItem("startInputValue", JSON.stringify(startInputValue))
+        localStorage.setItem("maxInputValue", JSON.stringify(maxInputValue))
     }
 
     // изменение стартового значения в настройках
